057843a5b9a50143eee35123ada0065ecbde2899
TildaDares <mathildaudufo@gmail.com>
2022-04-01 23:00:51 +0100

    send hello world

--- app.js
+++ app.js
@@ -34,6 +34,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
+app.get("/", (req, res, next) => res.send("Hello world"));
 app.use("/api", blogRouter);
 
 app.use(passport.initialize());
